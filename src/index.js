44a8989c7bb8478b3d4a6f5c4704cf2ace8568e3
Vinod Kumar <webindianlion@gmail.com>
2023-04-11 16:47:38 +0530

    Configure React app for deployment to GitHub

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Provider } from 'react-redux';
-// import { store } from './app/store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
